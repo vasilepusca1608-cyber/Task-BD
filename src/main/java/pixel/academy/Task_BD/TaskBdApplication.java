a1b023a051871a91d4d0ff81017af989ea204814
Pusca Vasile <vasilepusca1608@gmail.com>
2026-01-09 13:17:01 +0200

    Initial commit

---
+++ src/main/java/pixel/academy/Task_BD/TaskBdApplication.java
@@ -0,0 +1,13 @@
+package pixel.academy.Task_BD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskBdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pixel/academy/Task_BD/TaskBdApplicationTests.java
@@ -0,0 +1,13 @@
+package pixel.academy.Task_BD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskBdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
